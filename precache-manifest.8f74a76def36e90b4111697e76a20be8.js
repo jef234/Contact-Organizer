ef5da6ebff092ed1bf5987549ed65bdf97184dfe
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-07 20:34:04 +0530

    Updates

---
+++ precache-manifest.8f74a76def36e90b4111697e76a20be8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3874945d5e355cbeb4ade78fe0a60329",
+    "url": "/Contact-Organizer/index.html"
+  },
+  {
+    "revision": "71882f7c24b5c51cfdbd",
+    "url": "/Contact-Organizer/static/css/2.6f18c3fb.chunk.css"
+  },
+  {
+    "revision": "12ce88724dede6311904",
+    "url": "/Contact-Organizer/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "71882f7c24b5c51cfdbd",
+    "url": "/Contact-Organizer/static/js/2.55b9a00e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Organizer/static/js/2.55b9a00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ce88724dede6311904",
+    "url": "/Contact-Organizer/static/js/main.06055d4f.chunk.js"
+  },
+  {
+    "revision": "6f5c467aa6b542b324c8",
+    "url": "/Contact-Organizer/static/js/runtime-main.7f4a9589.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Contact-Organizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
