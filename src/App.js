a8447e659883e27e0fb53fc6d9745b89efe2d652
jr051323 <jeffrey.rajkumar@cerner.com>
2020-05-07 22:01:46 +0530

    Boilerplate for redux

--- src/App.js
+++ src/App.js
@@ -8,25 +8,30 @@ import Header from './Components/Layout/Header';
 import About from './Components/pages/About';
 import NotFound from './Components/pages/NotFound';
 
+import { Provider } from 'react-redux'
+import store from './Store/store'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header branding="Contact Organizer" />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Contacts} />
-            <Route exact path="/Contacts/Add" component={AddContact} />
-            <Route exact path="/Contacts/Edit/:id" component={EditContact} />
-            <Route exact path="/About" component={About} />
-            <Route exact component={NotFound} />
-          </Switch>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header branding="Contact Organizer" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/Contacts/Add" component={AddContact} />
+              <Route exact path="/Contacts/Edit/:id" component={EditContact} />
+              <Route exact path="/About" component={About} />
+              <Route exact component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   )
 }
 
